c009978341cf089cf1836c1cb65379849af20baf
RobsenGelana <robsengelana21@gmail.com>
2022-09-11 17:21:26 +0300

    fixing bug on decrypt function

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from sqlite3 import apilevel
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
  
-direction = input("To encrypt chose encode to decrypt chose decode: \n")
-text = input("Enter a text to encode: \n")
+direction = input("To encrypt chose encode to decrypt chose decode: \n").lower()
+text = input("Enter a text to encode: \n").lower()
 shift = int(input("Enter a number to shift: \n"))
 
 
@@ -17,4 +17,11 @@ def encrypt(plain_text, shift_amount):
     print(f"Your encode word is {encoded_word}")
 
 
-encrypt(plain_text=text, shift_amount=shift)
+def decrypt(plain_text, shift_amount):
+    decoded_word = ''
+    for char in plain_text:
+        position = alphabet.index(char)
+        new_position = position - shift
+        decoded_word += alphabet[new_position]
+    print(f"The decoded word is {decoded_word}")
+
