a6f4ab2daaf2de30848cb095883ee1e0921df441
RobsenGelana <robsengelana21@gmail.com>
2022-09-11 12:21:39 +0300

    adding two function for encrypt and decrypt

---
+++ main.py
@@ -0,0 +1,12 @@
+from art import logo
+
+def encrypt(plain_text, shift_amount):
+    pass
+
+def decrypt(plain_text, shift_amount):
+    pass
+
+
+direction = input("What do you want to do Type encode or decode: \n").lower
+text = input("Enter a text: \n").lower
+shift = int(input("Enter a shift number: "))
