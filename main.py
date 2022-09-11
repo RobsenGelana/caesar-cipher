e6a02922d531f95c9075497b22b302c6e084609f
RobsenGelana <robsengelana21@gmail.com>
2022-09-11 17:16:52 +0300

    fixing bug in encryped function

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-from art import logo
+from sqlite3 import apilevel
+
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+ 
+direction = input("To encrypt chose encode to decrypt chose decode: \n")
+text = input("Enter a text to encode: \n")
+shift = int(input("Enter a number to shift: \n"))
+
 
 def encrypt(plain_text, shift_amount):
-    pass
+    encoded_word = ''
+    for char in plain_text:
+        position = alphabet.index(char)
+        new_position = position + shift
+        encoded_word += alphabet[new_position]
+    print(f"Your encode word is {encoded_word}")
 
-def decrypt(plain_text, shift_amount):
-    pass
 
-
-direction = input("What do you want to do Type encode or decode: \n").lower
-text = input("Enter a text: \n").lower
-shift = int(input("Enter a shift number: "))
+encrypt(plain_text=text, shift_amount=shift)
